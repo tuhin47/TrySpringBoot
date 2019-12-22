181b3f2d0e0a99dd6e54a4214db55d31fd2fcdd6
Md. Towhidul Islam <tuhintowhidul9@gmail.com>
2019-12-22 21:45:39 +0600

    necessary to run the project in intellij in linux

--- src/main/java/com/tuhin/springboot/topic/TopicController.java
+++ src/main/java/com/tuhin/springboot/topic/TopicController.java
@@ -1,16 +1,9 @@
 package com.tuhin.springboot.topic;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.fasterxml.jackson.annotation.JsonRawValue;
+import java.util.List;
 
 @RestController
 public class TopicController {
@@ -23,7 +16,7 @@ public class TopicController {
 		return topicService.getTopics();
 	}
 	@RequestMapping("/topics/{id}")
-	public Topic addTopic(@PathVariable long id) {
+	public Topic getTopic(@PathVariable long id) {
 		return topicService.getTopic(id);
 	}
 
@@ -31,10 +24,10 @@ public class TopicController {
 	public void addTopic(@RequestBody Topic t) {
 		topicService.addTopic(t);
 	}
-	
+
 	@RequestMapping(method = RequestMethod.PUT,value="/topics")
 	public void updateTopic(@RequestBody Topic t) {
 		topicService.update(t);
 	}
-	
+
 }
