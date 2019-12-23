d5a9717e556502da36f3b4cda167fe9edf23c35b
MD. Towhidul Islam <tuhintowhidul9@gmail.com>
2019-12-23 13:14:56 +0600

    Implements ErrorController

---
+++ src/main/java/com/tuhin/springboot/index/IndexController.java
@@ -0,0 +1,23 @@
+package com.tuhin.springboot.index;
+
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController implements ErrorController {
+
+	
+	@Override
+	public String getErrorPath() {
+		// TODO Auto-generated method stub
+		return "/error";
+	}
+	
+	@RequestMapping("/error")
+	public String sayHi() {
+		
+		return "No Mapping Available";
+	}
+
+}
