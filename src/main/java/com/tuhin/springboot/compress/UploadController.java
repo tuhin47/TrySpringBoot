ddd4c7f62094766dfa29d9579c98284e0f3eb50a
MD. Towhidul Islam <tuhintowhidul9@gmail.com>
2020-02-10 11:29:23 +0600

    [Upload] upload a file

---
+++ src/main/java/com/tuhin/springboot/compress/UploadController.java
@@ -0,0 +1,48 @@
+package com.tuhin.springboot.compress;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+@Controller
+@RequestMapping("/files")
+public class UploadController {
+    @PostMapping("/upload")
+    public ResponseEntity uploadToLocalFileSystem(@RequestParam("file") MultipartFile file) {
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        Path path = Paths.get("G:\\temp\\" + fileName);
+        try {
+            Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/files/download/")
+                .path(fileName)
+                .toUriString();
+        return ResponseEntity.ok(fileDownloadUri);
+    }
+
+    @PostMapping("/multi-upload")
+    public ResponseEntity multiUpload(@RequestParam("files") MultipartFile[] files) {
+        List<Object> fileDownloadUrls = new ArrayList<>();
+        Arrays.asList(files)
+                .forEach(file -> fileDownloadUrls.add(uploadToLocalFileSystem(file).getBody()));
+        return ResponseEntity.ok(fileDownloadUrls);
+    }
+}
